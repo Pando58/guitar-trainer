6a82fa6c44fb8b98a2d778e9fdda454e0bac15c7
Enrique Pando <80338911+Pando58@users.noreply.github.com>
2022-08-07 23:12:44 -0500

    fix implicit type any for Select.svelte

--- src/components/Select.svelte
+++ src/components/Select.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let list: ArrayLike<unknown> = [];
-  export let selected;
+  export let selected: string;
 </script>
 
 <select bind:value={selected} class="bg-transparent m-1.5 pl-2 py-1 border-2 border-gray-400 text-gray-200 font-medium text-xs leading-tight uppercase rounded hover:bg-white hover:bg-opacity-10 focus:outline-none focus:ring-0 transition duration-75 ease-in-out">
