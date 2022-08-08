ddf3b80ec0fb9e1f36a5acf56f2ed40a64be083f
Enrique Pando <enr.pando@gmail.com>
2022-08-07 23:38:29 -0500

    set @ alias to src

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/guitar-trainer/',
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' }
+    ]
+  },
   plugins: [svelte()]
 })
