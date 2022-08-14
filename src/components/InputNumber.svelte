fd712485aec1553056a9adc3187bd7964bb5f1c6
Enrique Pando <enr.pando@gmail.com>
2022-08-13 21:48:57 -0500

    use prettier-plugin-svelte

--- src/components/InputNumber.svelte
+++ src/components/InputNumber.svelte
@@ -19,10 +19,10 @@
       value,
       update: (val: number) => {
         value = val;
-      }
+      },
     });
   }
-  
+
   onMount(() => {
     if (min !== null) input.min = min.toString();
     if (max !== null) input.max = max.toString();
@@ -30,4 +30,10 @@
   });
 </script>
 
-<input bind:this={input} bind:value on:change={updateInput} type="number" class={`${class_} bg-transparent m-1.5 px-2 py-1 border-2 border-gray-400 text-gray-200 font-medium text-xs leading-tight uppercase rounded hover:bg-white hover:bg-opacity-10 focus:outline-none focus:ring-0 transition duration-75 ease-in-out`}>
+<input
+  bind:this={input}
+  bind:value
+  on:change={updateInput}
+  type="number"
+  class={`${class_} bg-transparent m-1.5 px-2 py-1 border-2 border-gray-400 text-gray-200 font-medium text-xs leading-tight uppercase rounded hover:bg-white hover:bg-opacity-10 focus:outline-none focus:ring-0 transition duration-75 ease-in-out`}
+/>
