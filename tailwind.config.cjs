4dc3f102dc0f4122866225fa5249d55bdbd1ab75
Enrique Pando <enr.pando@gmail.com>
2022-08-14 18:07:38 -0500

    add settings button and sidebar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["index.html", "./src/**/*.{js,ts,svelte}"],
   theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         sans: ["Inter"],
