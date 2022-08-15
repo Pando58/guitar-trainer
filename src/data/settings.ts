4507d50c6334795da53580001dcc0071da7fc838
Enrique Pando <enr.pando@gmail.com>
2022-08-15 14:22:12 -0500

    move settingsStore values to settings

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,1 +1,54 @@
-export const intervalDisplayModes = ["full", "short", "abbreviated"];
+import { scaleNames } from "@/data/scales";
+
+interface select {
+  value: string;
+  list: string[];
+}
+
+interface inputNumber {
+  value: number;
+  min: number;
+  max: number;
+  step: number;
+}
+
+export type setting = select | inputNumber;
+
+const intervalDisplayModes = ["full", "short", "abbreviated"];
+
+const settings: {
+  selectedScale: select;
+  displayAmount: inputNumber;
+  nextNoteTimer: inputNumber;
+  intervalDisplayMode: select;
+} = {
+  selectedScale: {
+    value: scaleNames[0],
+    list: scaleNames,
+  },
+  displayAmount: {
+    value: 5,
+    min: 1,
+    max: 20,
+    step: 1,
+  },
+  nextNoteTimer: {
+    value: 5,
+    min: 0.1,
+    max: 60,
+    step: 0.1,
+  },
+  intervalDisplayMode: {
+    value: intervalDisplayModes[2],
+    list: intervalDisplayModes,
+  },
+};
+
+export default settings;
+
+export const {
+  selectedScale,
+  displayAmount,
+  nextNoteTimer,
+  intervalDisplayMode,
+} = settings;
