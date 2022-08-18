dd5d0ec888ce55815c418ce1a18aaf9694ab7a52
Pando58 <enr.pando@gmail.com>
2022-08-18 17:30:50 -0500

    settings menu titles

--- src/components/SidebarSettings.svelte
+++ src/components/SidebarSettings.svelte
@@ -10,46 +10,59 @@
   import settings from "@/data/settings";
 </script>
 
-<div
-  class="flex flex-col w-full sm:w-[32rem] px-2 xs:px-4 divide-y divide-black divide-opacity-20"
->
-  <div class="setting">
-    <span>{getEntry((e) => e.settings.scale.label)}</span>
-    <Select
-      displayList={Object.values(getEntries((e) => e.settings.scale.options))}
-      list={settings.selectedScale.list}
-      bind:selected={$selectedScale}
-      textTransform="capitalize"
-    />
-  </div>
-  <div class="setting">
-    <span>{getEntry((e) => e.settings.displayAmount)}</span>
-    <InputNumber
-      value={settings.displayAmount.value}
-      min={settings.displayAmount.min}
-      max={settings.displayAmount.max}
-      step={settings.displayAmount.step}
-      name={"displayAmount"}
-      on:updateInput={updateInput}
-      class_="w-14"
-    />
-  </div>
-  <div class="setting">
-    <span>{getEntry((e) => e.settings.intervalDisplayMode.label)}</span>
-    <Select
-      displayList={Object.values(
-        getEntries((e) => e.settings.intervalDisplayMode.options)
-      )}
-      list={settings.intervalDisplayMode.list}
-      bind:selected={$intervalDisplayMode}
-      textTransform="capitalize"
-    />
+<div class="flex flex-col w-full sm:w-[32rem] px-2 xs:px-4">
+  <span class="title">General</span>
+  <div class="group">
+    <div class="setting">
+      <span>{getEntry((e) => e.settings.scale.label)}</span>
+      <Select
+        displayList={Object.values(getEntries((e) => e.settings.scale.options))}
+        list={settings.selectedScale.list}
+        bind:selected={$selectedScale}
+        textTransform="capitalize"
+      />
+    </div>
+    <div class="setting">
+      <span>{getEntry((e) => e.settings.displayAmount)}</span>
+      <InputNumber
+        value={settings.displayAmount.value}
+        min={settings.displayAmount.min}
+        max={settings.displayAmount.max}
+        step={settings.displayAmount.step}
+        name={"displayAmount"}
+        on:updateInput={updateInput}
+        class_="w-14"
+      />
+    </div>
+    <div class="setting">
+      <span>{getEntry((e) => e.settings.intervalDisplayMode.label)}</span>
+      <Select
+        displayList={Object.values(
+          getEntries((e) => e.settings.intervalDisplayMode.options)
+        )}
+        list={settings.intervalDisplayMode.list}
+        bind:selected={$intervalDisplayMode}
+        textTransform="capitalize"
+      />
+    </div>
   </div>
 </div>
 
 <style>
+  .title {
+    @apply mb-3 mt-5 text-sm font-bold;
+  }
+
+  .group {
+    @apply px-2 py-1 bg-black bg-opacity-10 rounded-lg;
+  }
+
   .setting {
     @apply flex-1 flex justify-between items-center pl-4 py-1;
+  }
+
+  .setting:not(:first-child) {
+    @apply border-t border-black border-opacity-40;
   }
 
   .setting span {
