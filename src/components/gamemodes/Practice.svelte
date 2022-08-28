33f727b91740d061de8097118f8c6eb35895fa29
Enrique Pando <enr.pando@gmail.com>
2022-08-27 23:01:35 -0500

    practice mode component structure

--- src/components/gamemodes/Practice.svelte
+++ src/components/gamemodes/Practice.svelte
@@ -1,3 +1,37 @@
-<div class="w-full h-full flex items-center justify-center text-6xl text-white">
-  temp
+<script lang="ts">
+  import Button from "@/components/Button.svelte";
+  import { get } from "svelte/store";
+  import { noteGenerator } from "@/stores/noteGenerator";
+  import { displayAmount } from "@/stores/settingsStore";
+  import { counter } from "@/stores/displayCounter";
+  import { getEntry } from "@/utils/lang";
+  import IntervalDisplay from "@/components/IntervalDisplay.svelte";
+
+  $: counterLimit = $displayAmount + 20;
+
+  function pullNote() {
+    noteGenerator.pullNote();
+    if (get(counter) + 1 > counterLimit - 1) {
+      $counter = 0;
+    } else {
+      $counter++;
+    }
+  }
+
+  function btnReset() {
+    noteGenerator.reset();
+  }
+
+  function btnNext() {
+    pullNote();
+  }
+</script>
+
+<div class="flex-1 flex items-center justify-center flex-wrap gap-x-4 p-1.5">
+  <div>
+    <Button text={getEntry((e) => e.reset)} on:click={btnReset} />
+    <Button text={getEntry((e) => e.next)} on:click={btnNext} />
+  </div>
 </div>
+
+<IntervalDisplay {counterLimit} />
